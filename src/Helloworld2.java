c609590b32b1a6833188326c22a939969b690f11
kameugne <rkameugne@gmail.com>
2021-03-16 09:03:30 +0100

    add class triangle

--- src/Helloworld2.java
+++ src/Helloworld2.java
@@ -4,5 +4,8 @@ public class Helloworld2 {
         Rectangle rec=new Rectangle(30,15,"RECLudo");
         rec.affiche();
         System.out.println("Il a pour perimetre "+rec.perimetre()+" et pour surface "+rec.surface());
+        System.out.println("voici de nouvelle modifications que j'apporte a votre projet");
+        Triangle tri = new Triangle("triangle", 20.0, 15.5, 35.5);
+        System.out.println("Perimetre du triangle est : " + tri.perimetre());
     }
 }
